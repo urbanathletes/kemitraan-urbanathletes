48c595e0e2f2271de0df3317c6679654068f11b7
dnfebri <dwinofebri@gmail.com>
2022-07-26 14:05:23 +0700

    timetable

--- public/js/coba.js
+++
@@ -1,8 +0,0 @@
-// console.log('coba');
-// function viewDetail(element) {
-//   // let cardThis = element.parentNode.parentNode.parentNode.parentNode
-  
-//   console.log(cardThis.id('detail'));
-//   console.log('view detail', viewDetail);
-// }
-
