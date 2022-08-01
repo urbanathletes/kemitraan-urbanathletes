abd4ee67745a587c1015f761c76b4f9e47249424
dnfebri <dwinofebri@gmail.com>
2022-08-01 23:16:30 +0700

    done All page

---
+++ resources/views/public/kemitraan/presentation.blade.php
@@ -0,0 +1,120 @@
+<x-main_kemitraan>
+  <div class="flex justify-center max-h-[30vh] lg:max-h-[40vh] relative">
+    <img class="object-cover object-center w-full mx-auto" src="{{ url('image/web/bg_page/BGAboutUsDeskripsi.jpg') }}" alt="Store Urban Athletes">
+    <div class="absolute top-0 bottom-0 right-0 left-0 bg-black opacity-80"></div>
+    <div class="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-center items-center text-white">
+      <div class="absolute bottom-0 max-w-max text-center pb-6">
+        <h2 class="text-4xl font-FuturaBold pb-2 lg:pb-4">ANUAL REPORT</h2>
+      </div>
+    </div>
+  </div>
+
+  <div class="container my-8">
+    <div class="my-4 pl-4">
+      <h2 class="font-FuturaBold text-2xl px-4 max-w-max bg-yellow-primary mb-2">ANUAL REPORT</h2>
+      {{-- <p class="max-w-5xl tracking-widest">At Urban Athletes we have made it simple, affordable and convenient for everyone to achieve their personal health goals. Whether you’re a beginner or a pro, young or old, everybody is welcome.</p> --}}
+    </div>
+
+    <div class="mt-8 grid lg:grid-cols-2">
+      <div class="pl-4 space-y-12">
+        <div class="flex justify-start relative">
+          <div class="pl-4 py-1 border-l-4 border-yellow-primary flex items-center">
+            <div class="mx-4">
+              <img class="w-24" src="{{ url('image/web/logo/LOGO_UA_HITAM_TM.png') }}" alt="Urban Athletes">
+            </div>
+            <div>
+              <h3 class="font-FuturaBold text-lg">URBAN ATHLETES INVESTOR</h3>
+              <h3 class="font-FuturaBold text-lg">PRESENTATION</h3>
+              <div class="text-blue-500 tracking-widest space-x-4">
+                <a href=""><i class="fa-solid fa-file-powerpoint"></i> Presentation</a>
+                <a href=""><i class="fa-solid fa-file-pdf"></i> Report</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-start relative">
+          <div class="pl-4 py-1 border-l-4 border-yellow-primary flex items-center">
+            <div class="mx-4">
+              <img class="w-24" src="{{ url('image/web/logo/fitness_work-02.png') }}" alt="Urban Athletes">
+            </div>
+            <div>
+              <h3 class="font-FuturaBold text-lg">FITNESS WORKS INVESTOR</h3>
+              <h3 class="font-FuturaBold text-lg">PRESENTATION</h3>
+              <div class="text-blue-500 tracking-widest space-x-4">
+                <a href=""><i class="fa-solid fa-file-powerpoint"></i> Presentation</a>
+                <a href=""><i class="fa-solid fa-file-pdf"></i> Report</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {{-- <div class="pt-2 pb-8 space-y-4 relative">
+          <div class="absolute top-0 bottom-0 left-4 border-r-4 border-dashed border-black"></div>
+          <div class="ml-16 pl-6 py-1 border-l-2 border-yellow-primary">
+            <h3 class="font-FuturaBold text-lg">FITNESSWORKS, July 19<span class="ordinal text-sm inline-block align-top">th</span></h3>
+            <div class="text-blue-500 tracking-widest space-x-4">
+              <a href=""><i class="fa-solid fa-file-powerpoint"></i> Presentation</a>
+              <a href=""><i class="fa-solid fa-file-pdf"></i> Report</a>
+            </div>
+          </div>
+          <div class="ml-16 pl-6 py-1 border-l-2 border-yellow-primary">
+            <h3 class="font-FuturaBold text-lg">Document Name, July 10<span class="ordinal text-sm inline-block align-top">th</span></h3>
+            <div class="text-blue-500 tracking-widest space-x-4">
+              <a href=""><i class="fa-solid fa-file-powerpoint"></i> Presentation</a>
+              <a href=""><i class="fa-solid fa-file-pdf"></i> Report</a>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-start relative">
+          <div class="absolute top-0 bottom-0 left-0 w-8 h-8 bg-black border-8 border-yellow-primary z-10"></div>
+          <h2 class="font-FuturaBold text-xl ml-16">JUNE, 2022</h2>
+        </div>
+        <div class="pt-2 pb-8 space-y-4 relative">
+          <div class="absolute top-0 bottom-0 left-4 border-r-4 border-dashed border-black"></div>
+          <div class="tracking-widest ml-16 pl-6 py-1 border-l-2 border-yellow-primary">
+            <h3 class="font-FuturaBold text-lg">Document Name, June 19<span class="ordinal text-sm inline-block align-top">th</span></h3>
+            <div class="text-blue-500 space-x-4">
+              <a href=""><i class="fa-solid fa-file-powerpoint"></i> Presentation</a>
+              <a href=""><i class="fa-solid fa-file-pdf"></i> Report</a>
+            </div>
+          </div>
+          <div class="tracking-widest ml-16 pl-6 py-1 border-l-2 border-yellow-primary">
+            <h3 class="font-FuturaBold text-lg">Document Name, June 10<span class="ordinal text-sm inline-block align-top">th</span></h3>
+            <div class="text-blue-500 space-x-4">
+              <a href=""><i class="fa-solid fa-file-powerpoint"></i> Presentation</a>
+              <a href=""><i class="fa-solid fa-file-pdf"></i> Report</a>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-start relative">
+          <div class="absolute top-0 bottom-0 left-0 w-8 h-8 bg-black border-8 border-yellow-primary z-10"></div>
+          <h2 class="font-FuturaBold text-xl ml-16">MAY, 2022</h2>
+        </div>
+        <div class="pt-2 pb-8 space-y-4 relative">
+          <div class="absolute top-0 bottom-0 left-4 border-r-4 border-dashed border-black"></div>
+          <div class="tracking-widest ml-16 pl-6 py-1 border-l-2 border-yellow-primary">
+            <h3 class="font-FuturaBold text-lg">Document Name, May 24<span class="ordinal text-sm inline-block align-top">th</span></h3>
+            <div class="text-blue-500 space-x-4">
+              <a href=""><i class="fa-solid fa-file-powerpoint"></i> Presentation</a>
+              <a href=""><i class="fa-solid fa-file-pdf"></i> Report</a>
+            </div>
+          </div>
+          <div class="tracking-widest ml-16 pl-6 py-1 border-l-2 border-yellow-primary">
+            <h3 class="font-FuturaBold text-lg">Document Name, May 9<span class="ordinal text-sm inline-block align-top">th</span></h3>
+            <div class="text-blue-500 space-x-4">
+              <a href=""><i class="fa-solid fa-file-powerpoint"></i> Presentation</a>
+              <a href=""><i class="fa-solid fa-file-pdf"></i> Report</a>
+            </div>
+          </div>
+        </div> --}}
+      </div>
+      <div class="my-6 lg:my-0">
+        <h3 class="font-FuturaBold text-xl border-l-8 border-yellow-primary px-4">PREVIEW</h3>
+        <div class="max-w-2xl bg-blue-300 mx-auto my-4">
+          <iframe src="https://drive.google.com/file/d/1XyTWFgn_T2yQqh-NEXUz4fzUG-q0QMXO/preview" width="100%" height="540" allow="autoplay"></iframe>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</x-main_kemitraan>
--- resources/views/public/static/about/clubs.blade.php
+++ resources/views/public/static/about/clubs.blade.php
@@ -6,52 +6,204 @@
         <h2 class="text-4xl">OF URBAN ATHLETES</h2>
       </div>
       <div class="lg:text-xl font-black space-x-4 my-4">
-        <button class="hover:text-yellow-primary">Gunawangsa Merr</button>
-        <button class="hover:text-yellow-primary">Gunawangsa Tidar</button>
-        <button class="hover:text-yellow-primary">Marvell City</button>
-        <button class="hover:text-yellow-primary">Lenmarc Mall</button>
+        <button id="btnMenu-img" data-id="1" class="hover:text-yellow-primary">Gunawangsa Merr</button>
+        <button id="btnMenu-img" data-id="2" class="hover:text-yellow-primary">Gunawangsa Tidar</button>
+        <button id="btnMenu-img" data-id="3" class="hover:text-yellow-primary">Marvell City</button>
+        <button id="btnMenu-img" data-id="4" class="hover:text-yellow-primary">Lenmarc Mall</button>
       </div>
     </div>
     
-    <div class="grid grid-cols-2 md:grid-cols-4 my-8">
-      <div class="col-span-2 max-h-60 m-1 overflow-hidden">
-        <button id="image" class="h-full">
-          <img class="object-cover object-center w-full" src="{{ url('image/web/about/clubs/tidar/Tidar_1.jpg') }}" alt="Clubs Urban Athletes">
-        </button>
+    <div id='group-image' data-id="2">
+      <div class="grid grid-cols-2 md:grid-cols-4 my-8">
+        <div class="col-span-2 max-h-60 m-1 overflow-hidden">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center w-full" src="{{ url('image/web/about/clubs/tidar/Tidar_1.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full aspect-auto" src="{{ url('image/web/about/clubs/tidar/Tidar_2.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_3.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_4.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_5.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_6.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_7.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
       </div>
-      <div class="m-1">
-        <button id="image" class="h-full">
-          <img class="object-cover object-center h-full aspect-auto" src="{{ url('image/web/about/clubs/tidar/Tidar_2.jpg') }}" alt="Clubs Urban Athletes">
-        </button>
+    </div>
+    <div id='group-image' data-id="1">
+      <div class="grid grid-cols-2 md:grid-cols-4 my-8">
+        <div class="col-span-2 max-h-60 m-1 overflow-hidden">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-bottom w-full" src="{{ url('image/web/about/clubs/merr/slider4.JPG') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full aspect-auto" src="{{ url('image/web/about/clubs/merr/slider2.JPG') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/merr/slider3.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/merr/slider1.JPG') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/merr/slider5.JPG') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        {{-- <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/merr/Tidar_6.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/merr/Tidar_7.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div> --}}
       </div>
-      <div class="m-1">
-        <button id="image" class="h-full">
-          <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_3.jpg') }}" alt="Clubs Urban Athletes">
-        </button>
+    </div>
+    <div id='group-image' data-id="3">
+      <div class="grid grid-cols-2 md:grid-cols-4 my-8">
+        <div class="col-span-2 max-h-60 m-1 overflow-hidden">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center w-full" src="{{ url('image/web/about/clubs/marvell/marvell1.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full aspect-auto" src="{{ url('image/web/about/clubs/marvell/marvell2.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/marvell/marvell3.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/marvell/marvell4.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/marvell/marvell5.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/marvell/marvell6.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/marvell/marvell7.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
       </div>
-      <div class="m-1">
-        <button id="image">
-          <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_4.jpg') }}" alt="Clubs Urban Athletes">
-        </button>
-      </div>
-      <div class="m-1">
-        <button id="image">
-          <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_5.jpg') }}" alt="Clubs Urban Athletes">
-        </button>
-      </div>
-      <div class="m-1">
-        <button id="image">
-          <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_6.jpg') }}" alt="Clubs Urban Athletes">
-        </button>
-      </div>
-      <div class="m-1">
-        <button id="image">
-          <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/tidar/Tidar_7.jpg') }}" alt="Clubs Urban Athletes">
-        </button>
+    </div>
+    <div id='group-image' data-id="4">
+      <div class="grid grid-cols-2 md:grid-cols-4 my-8">
+        <div class="col-span-2 max-h-60 m-1 overflow-hidden">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center w-full" src="{{ url('image/web/about/clubs/lenmarc/lenmarc1.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full aspect-auto" src="{{ url('image/web/about/clubs/lenmarc/lenmarc2.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/lenmarc/lenmarc3.JPG') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/lenmarc/lenmarc4.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/lenmarc/lenmarc5.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/lenmarc/lenmarc6.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
+        <div class="m-1">
+          <button onclick="imageView(this)" class="h-full">
+            <img class="object-cover object-center h-full" src="{{ url('image/web/about/clubs/lenmarc/lenmarc7.jpg') }}" alt="Clubs Urban Athletes">
+          </button>
+        </div>
       </div>
     </div>
   </div>
   @push('script')
-    
+    <script src="/js/coba.js"></script>
+    <script>
+      function imageView (e) {
+        console.log(e);
+        Swal.fire({
+          html: e.innerHTML,
+          width:'95%',
+          // height:'100vh',
+          color: '#000',
+          background: '#E5E5E5',
+          showConfirmButton: false,
+          showCloseButton: true,
+        })
+      }
+      let btnMenuImage = document.querySelectorAll('#btnMenu-img')
+      let groupImage = document.querySelectorAll('#group-image')
+      btnMenuImage.forEach(menu => {
+        menu.addEventListener('click', function () {
+          groupImage.forEach(image => {
+            image.classList.add('hidden')
+            btnMenuImage.forEach(color => {
+              color.classList.remove('text-yellow-primary')
+            });
+          });
+          groupImage.forEach(image => {
+            if (image.getAttribute('data-id') == menu.getAttribute('data-id')) {
+              menu.classList.add('text-yellow-primary')
+              image.classList.remove('hidden')
+            }
+          });
+        })
+      });
+      
+    </script>
   @endpush
 </x-main_web>
