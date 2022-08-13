536dc9623d721831c1d792f77ab201e1d561b88b
Urban Athletes <93457663+urbanathletes@users.noreply.github.com>
2022-08-13 15:30:12 +0700

    Update TurnstileController.php

--- app/Http/Controllers/TurnstileController.php
+++ app/Http/Controllers/TurnstileController.php
@@ -21,7 +21,7 @@ class TurnstileController extends Controller
         if (!$cekMember1) {
             $apiModel = new ApiModel();
             $cekMemberApi = $apiModel->cekMember($request->rfid);
-            return $apiModel;
+            return $cekMemberApi;
 //             if (array_key_exists('member', $cekMemberApi)) {
 //                 try {
 //                     $saveData = DataMember::create([
